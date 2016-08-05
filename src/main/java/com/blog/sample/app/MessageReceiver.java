2e830da4c8b44c77e02075b781eaa2db8c36bbc8
brians java blog <briansjavablog1@gmail.com>
2016-08-05 07:48:49 +0100

    change to consume message using jmsTemplate rather than messageListenerCCoOntainer

--- src/main/java/com/blog/sample/app/MessageReceiver.java
+++ src/main/java/com/blog/sample/app/MessageReceiver.java
@@ -1,16 +1,25 @@
 package com.blog.sample.app;
 
-import org.springframework.jms.annotation.JmsListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageReceiver {
 
-	
-    @JmsListener(destination = "TestQueue", containerFactory = "jmsContainerFactory")
-    public String receiveMessage(String message) {
-        
-    	System.out.println("Received [" + message + "]");
-        return message;
-    }
+	private final JmsTemplate jmsTemplate;
+
+	@Autowired
+	public MessageReceiver(JmsTemplate jmsTemplate) {
+		this.jmsTemplate = jmsTemplate;
+	}
+
+	// @JmsListener(destination = "TestQueue", containerFactory =
+	// "jmsContainerFactory")
+	public String retrieveMessage() {
+
+		String message = (String) jmsTemplate.receiveAndConvert("TestQueue");
+		System.out.println("Received [" + message + "]");
+		return message;
+	}
 }
---
+++ src/main/java/com/blog/sample/app/SimpleMessageConverter.java
@@ -0,0 +1,31 @@
+package com.blog.sample.app;
+
+import javax.jms.JMSException;
+import javax.jms.MapMessage;
+import javax.jms.Message;
+import javax.jms.Session;
+
+import org.springframework.jms.support.converter.MessageConversionException;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleMessageConverter implements MessageConverter {
+
+	@Override
+	public Message toMessage(Object messageObject, Session session) throws JMSException, MessageConversionException {
+
+		String messageString = (String) messageObject;
+		MapMessage message = session.createMapMessage();
+		message.setString("message", messageString);
+		return message;
+	}
+
+	@Override
+	public Object fromMessage(Message message) throws JMSException, MessageConversionException {
+
+		MapMessage mapMessage = (MapMessage) message;
+		return mapMessage.getString("message");
+	}
+
+}
