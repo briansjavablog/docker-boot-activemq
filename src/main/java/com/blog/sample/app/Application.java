9b48bd97dc6b90d2efbc6d17d8ad2909995ed18b
briansjavablog <hannawaybrian@googlemail.com>
2016-08-11 17:40:52 +0100

    removed @EnableJms annotation

--- src/main/java/com/blog/sample/app/Application.java
+++ src/main/java/com/blog/sample/app/Application.java
@@ -2,10 +2,8 @@ package com.blog.sample.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
-@EnableJms
 public class Application {
     
     public static void main(String[] args) {
